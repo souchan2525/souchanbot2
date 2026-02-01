5e16b096ad2bb351a10b84d81b5f69ddad506d17
souchan2525 <color.sorairo.2525@gmail.com>
2026-02-01 19:34:00 +0900

    Update getmoney.js

--- commands/getmoney.js
+++ commands/getmoney.js
@@ -54,7 +54,7 @@ module.exports = {
         .setTitle("💰お金を追加しました！")
         .setDescription(`<@${user}> さんに ${money} コイン追加しました！`)
         .setFooter({ text: "詳しくは /inventory で確認してみてね！" });
-        .setColor("blue")
+        .setColor("Blue")
       if (money < 0) {
         embed.setColor("Red")
       } else if (money > 0) {
