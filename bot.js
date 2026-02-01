4dc0c77f822fb7f375a276c3da9f5d90cf11d0a0
souchan2525 <color.sorairo.2525@gmail.com>
2026-02-01 14:13:41 +0900

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config()
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+client.on("interactionCreate", async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (err) {
+        console.error(err);
+        interaction.reply({ content: "エラーが発生しました...", ephemeral: true });
+    }
+});
+
+client.once("ready", () => {
+    console.log(`ログイン完了: ${client.user.tag}`);
+});
+
+client.login(process.env.token);
