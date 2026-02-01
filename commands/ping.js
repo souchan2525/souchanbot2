c8c6c8f4752a04abf27d3f98e3d96f0738f5965d
souchan2525 <color.sorairo.2525@gmail.com>
2026-02-01 15:25:46 +0900

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require("discord.js");
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("botが生きてるか確認できるよ！")
+    async execute(interaction) {
+        if (!interaction.isChatInputCommand()) return;
+        try {
+            async execute(interaction) {
+              const sent = await interaction.reply({ content: "計測中...", fetchReply: true });
+              const ping = sent.createdTimestamp - interaction.createdTimestamp;
+              await interaction.editReply( `🏓 **Pong!**\n` + `メッセージ応答速度: **${ping}ms**\n` ); }
+            }
+        } catch (er) {
+            console.error("エラー内容:" + er)
+        }
+    }
+};
