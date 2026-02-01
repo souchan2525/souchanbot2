f50f9e044a3c660eda610bb838772229daa25a44
souchan2525 <color.sorairo.2525@gmail.com>
2026-02-01 19:04:19 +0900

    Enhance ping command with a description
    Add a description to the ping command for clarity.

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,8 @@
 const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("botが生きてるか確認できるよ！")
     async execute(interaction) {
         if (!interaction.isChatInputCommand()) return;
         try {
