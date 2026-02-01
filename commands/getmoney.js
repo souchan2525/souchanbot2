896f6b3f79db324d8a6daf937ff1ed80f75621a6
souchan2525 <color.sorairo.2525@gmail.com>
2026-02-01 18:53:11 +0900

    Create getmoney.js

---
+++ commands/getmoney.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fs = require("fs");
+const path = require("path")
+const data = JSON.parse(fs.readFileSync(__dirname + "/data.json", "utf8"));
+module.exports = {
+    async execute(interaction) {
+        if (!interaction.isChatInputCommand()) return;
+        try {
+            if (interaction.commandName === "moneyget") {
+                const userid = interaction.user.id
+                const adminlist = [830518901221490740, 1395712192795512902]
+                for (ad of adminlist) {
+                    if (ad === userid) {
+                        const money = interaction.options.getInteger("balance")
+                        const user = interaction.options.getInteger("user")
+                        data[user].money += parseInt(money)
+                        fs.writeFileSync(__dirname + "/data.json", "utf8"), JSON.stringify(data, null, 2), "utf8");
+                        const embed = new EmbedBuilder()
+                            .setTitle("💰お金を追加しました！")
+                            .setDescription("<@${user}>さんに${money}コイン追加しました！")
+                            .setFooter({
+                                text: "詳しくは/inventoryで確認してみてね！"
+                            })
+                        interaction.reply({
+                            embeds: [embed]
+                        })
+                    } else {
+                        interaction.reply({
+                            content: "これはbot管理者のみ実行できます！", ephemeral: true
+                        })
+                    }
+                }
+            }
+        } catch (er) {
+            console.error("エラー内容:" + er)
+        }
+    }
+};
