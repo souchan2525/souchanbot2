6d93e9803e6ed2661e7070c8aff7f5f17fd65bcf
souchan2525 <color.sorairo.2525@gmail.com>
2026-02-02 19:53:18 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ require("dotenv").config()
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const { createClient } = require("@supabase/supabase-js");
 const supabase = createClient( process.env.baseurl, process.env.basekey );
+module.exports = { supabase };
 
 const client = new Client({
     intents: [
@@ -58,6 +59,7 @@ app.listen(3000, () => {
 });
 
 client.login(process.env.token);
+
 
 
 
