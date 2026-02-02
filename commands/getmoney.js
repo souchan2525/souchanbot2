f4947eb20dfce943e4940323785b22ef9d4a7a7d
souchan2525 <color.sorairo.2525@gmail.com>
2026-02-02 19:35:31 +0900

    Update getmoney.js

--- commands/getmoney.js
+++ commands/getmoney.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.baseurl,
+  process.env.basekey
 );
 
 module.exports = {
