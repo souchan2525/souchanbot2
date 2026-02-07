1c0bdf382daf0ae34858a7ed3d508c8eb2bf2a5c
souchan2525 <color.sorairo.2525@gmail.com>
2026-02-07 18:40:33 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,9 +28,7 @@ const commands = [
       await interaction.reply({ content: "送るよ！", ephemeral: true })
       await interaction.followUp(
 `${text}
-\`
-送信者: ${interaction.user.username}
-\`
+\`送信者: ${interaction.user.username}\`
 `
       );
     }
@@ -171,6 +169,7 @@ client.on("interactionCreate", async interaction => {
 //  ログイン
 // ===============================
 client.login(process.env.token);
+
 
 
 
